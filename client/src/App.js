2dffd381848f13a8dde36888bf7c89c129cd0df1
RAFET <75682122+cirpanrafet@users.noreply.github.com>
2022-11-25 22:11:37 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React, { Fragment } from "react";
 import './App.css';
 
+
 import InputClub from "./compenents/InputClub";
 import ListClubs from "./compenents/ListClubs";
 import InputEvent from "./compenents/InputEvent";
 import ListEvents from "./compenents/ListEvents";
-
 function App() {
   return <Fragment>
           <div className="container-fluid">
             <InputClub/>
             <br></br>
             <ListClubs />
+            
             <InputEvent/> 
             <br></br>
             <ListEvents />
