59eb4dc389e382bc35a73ccadfd0f355f75a4390
RAFET <75682122+cirpanrafet@users.noreply.github.com>
2022-11-25 22:05:29 +0300

    Update EditEvent.js

--- client/src/compenents/EditEvent.js
+++ client/src/compenents/EditEvent.js
@@ -1,31 +1,31 @@
-
 import React, {Fragment, useState} from "react";
 
 const EditEvent = ({event}) => {
-    const [event_details, setEvents] = useState(event.event_details);
+    const [event_details, setEventdetails] = useState(event.event_details);
     
-    //edit edit_details function
+    //edit name function
 
-    const updateEvents = async(a) => {
-        a.preventDefault();
+    const updateEventdetails = async(e) => {
+        e.preventDefault();
         try {
             const body = {event_details};
-            const response = await fetch(`http://localhost:5000/event/${event.event_id}`, {
+            const response = await fetch(`http://localhost:5000/events/${event.event_id}`, {
                 method:"PUT",
                 headers: {"Content-Type": "application/json"},
                 body: JSON.stringify(body)
-            });
-            window.location = "/"
+            }
+          );
+          window.location = "/";
         } catch (err) {
             console.error(err.message);
             
         }
-    };
+      };
 
     return ( 
     <Fragment>
-    <button type="button" class="btn btn-warning" data-toggle="modal" data-target={`#id${event.event_id}`}>
-        Düzenle
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target={`#id${event.event_id}`}>
+      Etkinlik Düzenle
     </button>
     {/*
         id = id10
@@ -33,26 +33,26 @@ const EditEvent = ({event}) => {
     <div    
         class="modal" 
         id={`id${event.event_id}`}
-        onClick = {() => setEvents(event.event_details)}>
+        onClick = {() => setEventdetails(event.event_details)}>
       <div class="modal-dialog">
         <div class="modal-content">
     
           
           <div class="modal-header">
-            <h4 class="modal-title">Edit Event</h4>
-            <button type="button" class="close" data-dismiss="modal" onClick = {() => setEvents(event.event_details)}>&times;</button>
+            <h4 class="modal-title">Toplantı İçeriğini Düzenle</h4>
+            <button type="button" class="close" data-dismiss="modal" onClick = {() => setEventdetails(event.event_details)}>&times;</button>
           </div>
     
           
           <div class="modal-body">
-            <input type="text" className="form-control" value={event_details} onChange={a =>
-            setEvents(a.target.value)}/>
+            <input type="text" className="form-control" value={event_details} onChange={e => //3 saat sonunda çözülen hata value içi :)))
+            setEventdetails(e.target.value)}/>
           </div>
     
           
-          <div class="modal-footer">
-            <button type="button" class="btn btn-warning" data-dismiss="modal" onClick = {a => updateEvents(a)} >Edit</button>
-            <button type="button" class="btn btn-danger" data-dismiss="modal" onClick = {() => setEvents(event.event_details)}>Close</button>
+          <div class="modal-header">
+            <button type="button" class="btn btn-warning" data-dismiss="modal" onClick = {e => updateEventdetails(e)} >Düzenle</button>
+            <button type="button" class="btn btn-danger" data-dismiss="modal" onClick = {() => setEventdetails(event.event_details)}>Kapat</button>
           </div>
     
         </div>
