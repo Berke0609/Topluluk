3cdc0d95278378567320c3a9c198d8d5cd5f996e
RAFET <75682122+cirpanrafet@users.noreply.github.com>
2022-11-25 22:15:37 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "12345",
+    password: "PGADMIN GİRİŞİNDEKİ ŞİFRENİZİ YAZMANIZ GEREKİYOR:",
     host: "localhost",
     port: 5432,
-    database: "hacettepekulupler"
+    database: "PGADMINDE OLUŞTURDUĞUNUZ DATABASE ADINI YAZMANIZ GEREKİYOR:"
 
 })
 
